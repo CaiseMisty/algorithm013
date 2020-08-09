e5e22c1c775912ba4438dfbe5fed84ec477fb8e3
Liuziao <burningalive.fff@live.com>
2020-08-10 03:05:38 +0800

    docs: ✏️ 添加注释

--- Week_02/347-前k个高频元素-top-k-frequent-elements.ts
+++ Week_02/347-前k个高频元素-top-k-frequent-elements.ts
@@ -1,5 +1,11 @@
 import { Heap } from '$utils/index';
 
+/**
+ * 用堆 书写代码的难度不大..  主要的难点是上面引入的这个堆的实现,
+ * 这道题不能像老师在视频里讲的那种简单实现 Heap<int> 来做
+ * 所以实现了个带泛型, 可以指定是小顶堆大顶堆, 类似java PreorityQueue初始化时传入自定义比较函数
+ */
+
 function topKFrequent(nums: number[], k: number): number[] {
   const map = new Map();
   for (let num of nums) {
